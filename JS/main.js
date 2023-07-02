3a7c9ca6f85b6e226de1d166c885f659cc336501
joyhyunki <91402570+joyhyunki@users.noreply.github.com>
2023-07-03 01:12:07 +0900

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,17 @@
+var myArray = ["Prehistoric", "Medieval", "Modern", "Nuclear", "Space"]
+var agenum = 0
+var agename = ""
 
+console.log("Cheated Civilization will be destroyed soon.");
+var age = document.getElementById("agetext");
+
+window.onkeypress = function(event) {
+    if (event.which == 32) {
+        agenum += 1;
+        if (agenum > 4) {
+            agenum = 0;
+        }
+        agename = myArray[agenum];
+        age.innerText = agename;
+    }
+}
