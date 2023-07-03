c318b617679817e0269fe47aa2b858cf4b032fb3
joyhyunki <91402570+joyhyunki@users.noreply.github.com>
2023-07-03 18:06:46 +0900

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,13 @@
 var myArray = ["Prehistoric", "Medieval", "Modern", "Nuclear", "Space"]
 var agenum = 0
 var agename = ""
+var aminoacidnum = document.getElementById("aminoacid");
+var aminoacid = 0
+var aminobutton = document.getElementById("aminobutton");
+var explainclick = document.getElementById("explainclick");
+var aminoexplain = `Gather Amino Acids
+The primary source for making the fundamental blocks of life.`;
+var eventlog = document.getElementById("chat");
 
 console.log("Cheated Civilization will be destroyed soon.");
 var age = document.getElementById("agetext");
@@ -15,3 +22,44 @@ window.onkeypress = function(event) {
         age.innerText = agename;
     }
 }
+
+aminobutton.onclick = function(){
+    aminoacid += 1;
+    aminoacidnum.innerText = aminoacid;
+};
+
+aminobutton.onmouseover = function(){
+    explainclick.innerText = aminoexplain;
+};
+
+aminobutton.onmouseout = function(){
+    explainclick.innerText = "";
+};
+
+eventlog.innerText = `HEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HElloHEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HElloHElloHEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HEllo
+HElloHElloHElloHEllo`
